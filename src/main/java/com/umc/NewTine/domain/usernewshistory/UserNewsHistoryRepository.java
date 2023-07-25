7edd036367fc5e075c8c076b82a8061b4aae2f45
KimDoUk <zjaxj230@naver.com>
2023-07-25 12:20:45 +0900

    [feat] UserNewsHistoryRepository 생성

---
+++ src/main/java/com/umc/NewTine/domain/usernewshistory/UserNewsHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.umc.NewTine.domain.usernewshistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserNewsHistoryRepository extends JpaRepository<UserNewsHistory, Long> {
+
+}
